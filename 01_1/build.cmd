a068919ff70d28b2d93f66ec869559da6919bacd
Artur Mazgarov <artur.mazgarov@skuvault.com>
2020-11-04 20:25:05 +0500

    Boost root is not needed

--- 01_1/build.cmd
+++ 01_1/build.cmd
@@ -2,7 +2,6 @@ rmdir /S /Q build
 mkdir build
 cd build
 set TRAVIS_BUILD_NUMBER=1.1
-::set BOOST_ROOT=C:\Projects\vcpkg-master\installed\x64-windows
 cmake .. -DCMAKE_TOOLCHAIN_FILE=C:\Projects\vcpkg-master\scripts\buildsystems\vcpkg.cmake
 
 :: build
