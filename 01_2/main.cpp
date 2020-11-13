483cc881ef8b4561df2aa4a3d18f5f81d5c63db9
Artur Mazgarov <artur.mazgarov@skuvault.com>
2020-11-13 11:49:35 +0500

    Change stdout from text mode to binary mode to avoid CR+LF line endings

--- 01_2/main.cpp
+++ 01_2/main.cpp
@@ -1,11 +1,17 @@
 #include "ip_filter.h"
 
 #include <iostream>
+#include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
 
 int main(int argc, char const *argv[])
 {
 	try
 	{
+		// change stdout from text mode to binary mode to avoid CR+LF line endings
+		// https://docs.microsoft.com/ru-ru/cpp/c-runtime-library/reference/setmode?view=msvc-160
+		_setmode( _fileno( stdout ), _O_BINARY );
 		ip_filter::read_and_process();
 	}
 	catch (const std::exception &e)
