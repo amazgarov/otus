aad368621430640da3d5a7fb0414be86a5a1e404
Artur Mazgarov <artur.mazgarov@skuvault.com>
2020-11-13 12:04:45 +0500

    Testing by using CMake

--- 01_2/tests.cmd
+++ 01_2/tests.cmd
@@ -1,4 +1,12 @@
 cd build/debug
+
+:: by using tests executable
 tests
+
+:: starndard way
 ::cmake --build . --target RUN_TESTS
+
+:: by using custom target
+::cmake --build . --target check
+
 cd ../../
